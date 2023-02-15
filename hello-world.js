f99c471647b77bc2fe76c80757682775555ed00d
Leonardo Muller <leowmuller@gmil.com>
2023-02-14 22:54:26 -0300

    é quadrado?

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,22 @@
 console.log("Helo World!");
 console.log("Oi, cara de boi");
+
+let ladoA = 1;
+let ladoB = 1;
+let ladoC = 2;
+let ladoD = 2;
+
+
+if (ladoA === ladoB) {
+    if (ladoB === ladoC) {
+        if (ladoC === ladoD) {
+            console.log("É quadrado")
+        } else {
+            console.log("Não é quadrado")
+        }
+    } else {
+        console.log("Não é quadrado")
+    }
+} else {
+    console.log("Não é quadrado")
+}
